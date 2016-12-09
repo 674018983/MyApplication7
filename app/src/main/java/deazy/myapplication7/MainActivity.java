e4017ccbff5d28c3f94aa8cfe96872fb03fcc638
XZC <674018983@qq.com>
2016-12-09 21:11:52 +0800

    first commit

---
+++ app/src/main/java/deazy/myapplication7/MainActivity.java
@@ -0,0 +1,16 @@
+package deazy.myapplication7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rey.material.widget.ProgressView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
